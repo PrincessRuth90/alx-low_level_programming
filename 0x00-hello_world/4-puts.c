2ed7683121a917542ec16c66ff0b47faa02319d1
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-02 16:45:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - return the integal of the function
- *Return: value of 0
+ *main - program prints with function puts
+ *
+ *Return: Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
