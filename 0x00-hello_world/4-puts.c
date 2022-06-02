df2ddc55cb796fbdadeca0bee1fd9f830c848cbf
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-02 10:28:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,12 +7,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingu
- *              l puzzle, followed by a new line.
+ *                l puzzle, followed by a new line.
  *
- *              Return: Always 0.
+ *           Return: Always 0.
  */
 int main (void)
-{	  
-   puts("\"Programming is like building a multilingual puzzle"); 
-   return (0);
-}       
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}  
