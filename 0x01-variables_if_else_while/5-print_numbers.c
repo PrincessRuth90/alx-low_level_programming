34b52f2c7f28b36b8c06af1f1767346452152abf
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-03 05:28:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints out all the numbers
+ * between 0 and 9
+ * Return: Always 0 (Success)
+ */
+int main(0)
+{
+int l;
+
+for (l = 0; l < 10; l++)
+printf("%d", l);
+
+printf("\n");
+return (0);
+}
