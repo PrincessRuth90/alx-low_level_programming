b7d59aa0b7b9a624f4c24e4301f2cf5e78197edd
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-03 07:04:28 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,17 @@
 
 /**
  * main - Prints out all the numbers
- * from 0 to 9, with putchar
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
 
-/* Prints out all the digits from 0 */
-/* to 9 after initializing l to the */
-/* ASCII value of '0' */
-for (l = '0'; l <= '9'; l++)
-putchar(l);
-
+for (i = 0; l < 9; l++)
+{
+putchar(i);
+}
 putchar ('\n');
 return (0);
 }
