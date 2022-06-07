bf8f3887de1cff629b14600dd1fd7a4255cebc39
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-07 07:50:03 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,20 @@
-#include "alx.h"
+#include "main.h"
 
-/*
- * print_alphabet_x10 - prints alphabet 10 times
- * Return:void
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
  */
+void print_alphabet_x10(void)
+{
+int a, b;
 
-void print_alphabet_x10(void);
+for (a = 0; a <= 9; a++)
 {
-char c;
-int i = 0
-
-while (i < 10)
+for (b = 97; b <= 122; b++)
 {
-c = 'a';
-while (c <= 'z')
-{
-_putchar(c);
-c++;
+_putchar(b);
 }
 _putchar('\n');
-i++;
 }
-
 }
