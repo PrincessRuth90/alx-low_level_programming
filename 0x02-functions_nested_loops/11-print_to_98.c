253eec7c96079b099ff22450a6cbf50aa7f89438
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-07 14:54:10 -0700

    98 Battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,25 @@
 #include "holberton.h"
-
+#include "main.h"
 /**
- * main - Check the code
- *
- * Return: Always 0.
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return: 0 or 1
  */
-int main(void)
+
+void print_to_98(int n)
 {
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
+
+while (n < 98)
+{
+printf("%i, ", n);
+n++;
+}
+while (n > 98)
+{
+printf("%i, ", n);
+n--;
+}
+
+printf("98");
+putchar('\n');
 }
