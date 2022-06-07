16c7ef13ec3855bdff0568d38c83e88360803ab2
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-07 13:38:31 -0700

    98 Battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,16 @@ void print_to_98(int n)
 if (n > 98)
 {
 for (; n > 98; n--)
-{
-printf("%d, ", n);
+_putchar("%d, ", n);
 }
 }
 else if (n < 98)
 {
 for (; n < 98; n++)
 {
-printf("%d, ", n);
+_putchar("%d, ", n);
 }
 }
-printf("%d, ", n);
+_putchar("%d, ", n);
 }
 
