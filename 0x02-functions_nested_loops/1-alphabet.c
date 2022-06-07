c56bd6b8914227dbc5081b97772542f7a306350b
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-07 00:59:47 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Print_alphabet - Entry point
+ * Return 0 (Always Success)
+ */
+void print_alphabet(void);
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+
+}
+_putchar('\n');
+
+}
+
