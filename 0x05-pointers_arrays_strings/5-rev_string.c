226732a44c21e22e32a5710dda8ab2f4441d1ee1
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-13 00:18:26 -0700

    Print reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,16 +2,18 @@
 /**
  * rev_string - prints reverse string
  * @s: pointer to the string to print
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void rev_string(char *s)
 {
+
 int a, b, c;
 char x;
 
 for (a = 0; s[a] != '\0'; a++)
 ;
+
 c = a;
 b = 0;
 for (c = c - 1; b < (c / 2); c--, b++)
