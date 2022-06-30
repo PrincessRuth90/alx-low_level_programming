b1cc4ae94e3716b118c893047fccbb6712c8f1aa
PrincessRuth90 <marcusruth86@gmail.com>
2022-06-30 10:11:22 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
+void errors(void);
+int _strlen(char *s);
+int _putchar(char c);
 
 #endif /*MAIN_H */
