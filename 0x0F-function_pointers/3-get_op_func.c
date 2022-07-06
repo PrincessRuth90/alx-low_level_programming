1c5343dc2fb2148dc3c3d8f226f6b230c6ea89c7
PrincessRuth90 <marcusruth86@gmail.com>
2022-07-06 14:37:20 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * get_op_func - matches operator from main
- * @s: op str
- * Return: 0
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -16,14 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-
 	int i = 0;
 
-	while (ops[i].op)
+	while (i < 10)
 	{
-		if (*ops[i].op == *s)
-			return (ops[i].f);
+		if (s[0] == ops->op[i])
+			break;
 		i++;
 	}
-	return (NULL);
+
+	return (ops[i / 2].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+
 /**
- * main- Entry point
- * Description: this program references 4 arguments
- * defined in 3-op_functions.c without callig them directly.
- * it has  3 exit statuses
- * 98 - if the arguments are not 4
- * 99 - if op is null
- * 100- if div or mod gives an answer 0
-* @argc: size of argv
- * @argv: array of size argc
- * Return: 0
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -25,25 +18,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	print("%d\n", ans);
+	printf("%d\n", operation(a, b));
 	return (0);
 }
