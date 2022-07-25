d05aa03b0053ba2b45217602c7b9f62ac6fb40a8
PrincessRuth90 <marcusruth86@gmail.com>
2022-07-25 15:49:26 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,38 +7,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-int file;
-ssize_t rcount, wcount;
-char *buffer;
+	if (!filename)
+		return (0);
 
-if (filename == NULL)
-	return (0);
+	fd = open(filename, O_RDONLY);
 
-file = open(filename, O_RDWR);
-if (file == -1)
-	return (0);
+	if (fd == -1)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-	free(buffer);
-	return (0);
-}
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-rcount = read(file, buffer, letters);
-if (rcount == -1)
-	return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-wcount = write(STDOUT_FILENO, buffer, rcount);
+	close(fd);
 
-if (wcount == -1 || rcount != wcount)
-	return (0);
+	free(buf);
 
-free(buffer);
-
-close(file);
-
-return (wcount);
+	return (nwr);
 }
 
