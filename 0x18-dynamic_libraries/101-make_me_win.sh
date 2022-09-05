61632663cd8b7e2ce45ddc97fd4d87b347b4ac7b
PrincessRuth90 <marcusruth86@gmail.com>
2022-09-05 14:04:56 -0700

    Jacpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/PrincessRuth90/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libmyprintf.so
-export LD_PRELOAD=/tmp/libmyprintf.so
+wget -P /tmp/ https://raw.github.com/PrincessRuth90/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
