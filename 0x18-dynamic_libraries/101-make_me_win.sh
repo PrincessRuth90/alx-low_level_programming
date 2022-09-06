a54aa2e2343edb1bacc63ad098cc15641a016b56
PrincessRuth90 <marcusruth86@gmail.com>
2022-09-05 19:50:51 -0700

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/PrincessRuth90/alx-low_level_programming/tree/master/0x18-dynamic_libraries/preload.so
+wget -P /tmp https://github.com/PrincessRuth90/alx-low_level_programming/blob/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
